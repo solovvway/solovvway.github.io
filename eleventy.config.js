22dbf773e60dc24494c0865bfa05c577d8acfad5
michail_laptop <saintsdertr@gmail.com>
2025-07-16 19:18:22 +0300

    try with react md engine

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,20 +1,9 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(syntaxHighlight);
+  // Remove or comment out the syntax highlight plugin
+  // eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-syntaxhighlight"));
 
   // Passthrough copy for images
   eleventyConfig.addPassthroughCopy("src/posts/*/images/*.{jpg,png,gif}");
-
-  // Custom filter for code highlighting
-  eleventyConfig.addFilter("highlightCode", function(content) {
-    const hljs = require("highlight.js");
-    return content.replace(/<pre><code( class="language-.*?")?>([\s\S]*?)<\/code><\/pre>/g, (match, lang, code) => {
-      const language = lang ? lang.replace(' class="language-', '').replace('"', '') : 'text';
-      const highlighted = hljs.highlight(code, { language }).value;
-      return `<pre><code class="language-${language}">${highlighted}</code></pre>`;
-    });
-  });
 
   // Collection for posts
   eleventyConfig.addCollection("posts", function(collectionApi) {
@@ -27,7 +16,8 @@ module.exports = function(eleventyConfig) {
       output: "_site",
       includes: "_includes"
     },
-    markdownTemplateEngine: "njk",
+    // Disable Markdown processing
+    markdownTemplateEngine: false,
     templateFormats: ["md", "njk", "html"]
   };
 };
