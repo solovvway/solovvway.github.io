628db676288cd0b4474fc8b7f6a727724052adac
michail_laptop <saintsdertr@gmail.com>
2025-07-23 16:20:42 +0300

    8

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://solovvway.github.io/", // replace this with your deployed domain
   author: "Sat Naing",
-  profile: "https://satnaing.dev/",
+  profile: "https://solovvway.github.io/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
   title: "AstroPaper",
   ogImage: "astropaper-og.jpg",
